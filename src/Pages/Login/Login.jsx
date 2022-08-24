b5206a64699cd89fee40f7c294e12e9fba1ec27a
Deep Shah <deepshah1358@gmail.com>
2022-08-24 23:55:26 +0530

    Firebase Google Login setup completed

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -10,9 +10,10 @@ import {
 } from "@mantine/core";
 import { useMediaQuery } from "@mantine/hooks";
 import { useStyles } from "./Login.style";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { Auth, googleAuthProvider } from "../../firebase";
 
 const Login = () => {
-
 	const { classes } = useStyles();
 
 	const [title, setTitle] = useState("Welcome Back To");
@@ -32,7 +33,23 @@ const Login = () => {
 			process.env.NODE_ENV === "development"
 				? "http://localhost:5000/api/auth/google"
 				: "https://my-expense-tracker-backend.herokuapp.com/api/auth/google";
-		window.open(BACKEND_URL, "_self");
+		signInWithPopup(Auth, googleAuthProvider)
+			.then((result) => {
+				// This gives you a Google Access Token. You can use it to access the Google API.
+				const credential = GoogleAuthProvider.credentialFromResult(result);
+				const token = credential.accessToken;
+				// The signed-in user info.
+				const user = result.user;
+			})
+			.catch((error) => {
+				// Handle Errors here.
+				const errorCode = error.code;
+				const errorMessage = error.message;
+				// The email of the user's account used.
+				const email = error.customData.email;
+				// The AuthCredential type that was used.
+				const credential = GoogleAuthProvider.credentialFromError(error);
+			});
 	};
 
 	return (
--- src/store/user/ThunkFunctions/fetchUser.js
+++ src/store/user/ThunkFunctions/fetchUser.js
@@ -2,11 +2,11 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "../../../../src/axios";
 import { error } from "./commonError";
 
-export const fetchUser = createAsyncThunk("user/fetchUser", async () => {
-  try {
-    const res = await axios.get("/auth/user");
-    return res.data;
-  } catch (err) {
-    return error(err);
-  }
+export const fetchUser = createAsyncThunk("user/fetchUser", async (email) => {
+	try {
+		const res = await axios.post("/auth/user", { email });
+		return res.data;
+	} catch (err) {
+		return error(err);
+	}
 });
