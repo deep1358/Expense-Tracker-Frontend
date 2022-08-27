295f8fc81b8a2ec388137bcd26cc6610e24a313a
Deep Shah <deepshah1358@gmail.com>
2022-08-27 20:01:36 +0530

    Fixed backend Url in env file

--- src/axios.js
+++ src/axios.js
@@ -3,10 +3,7 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 const customAxios = axios.create({
-	baseURL:
-		process.env.NODE_ENV === "development"
-			? process.env.REACT_APP_DEV_BACKEND_URL
-			: process.env.REACT_APP_PROD_BACKEND_URL,
+	baseURL: process.env.REACT_APP_BACKEND_URL,
 	timeout: 10000,
 });
 
