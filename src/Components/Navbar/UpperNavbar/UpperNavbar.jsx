33809245710e1546c66cf1960817d433c6abfffa
deep1358 <deepshah1358@gmail.com>
2022-12-17 14:50:33 +0530

    Fix navbar issue

--- src/Components/Navbar/UpperNavbar/UpperNavbar.jsx
+++ src/Components/Navbar/UpperNavbar/UpperNavbar.jsx
@@ -13,87 +13,91 @@ import { MakeUnAuthenticated, loggingOutToggler } from "../../../store/user";
 import { showNotification } from "@mantine/notifications";
 
 const UpperNavbar = ({ opened, setOpened }) => {
-	const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-	const { deletingUser, loggingOut } = useSelector((state) => state.user);
+    const { deletingUser, loggingOut } = useSelector((state) => state.user);
 
-	const smallScreen = useMediaQuery("(max-width: 710px)");
+    const smallScreen = useMediaQuery("(max-width: 775px)");
 
-	const { classes } = useStyles({ opened });
+    const { classes } = useStyles({ opened });
 
-	const [deleteConfirmBoxOpened, deleteConfirmBoxToggleOpened] =
-		useState(false);
+    const [deleteConfirmBoxOpened, deleteConfirmBoxToggleOpened] =
+        useState(false);
 
-	useEffect(() => {
-		dispatch(toggleLoadingOverlay(loggingOut || deletingUser));
-	}, [loggingOut, deletingUser]);
+    useEffect(() => {
+        dispatch(toggleLoadingOverlay(loggingOut || deletingUser));
+    }, [loggingOut, deletingUser]);
 
-	const handleLogoutUser = (message) => {
-		dispatch(loggingOutToggler(true));
+    const handleLogoutUser = (message) => {
+        dispatch(loggingOutToggler(true));
 
-		// get current seconds since epoch to use as a unique id
-		const getCurrentSeconds = () => Math.floor(Date.now() / 1000);
-		signOut(Auth)
-			.then(() => {
-				if (message) {
-					// Sign-out successful.
-					dispatch(MakeUnAuthenticated());
-					showNotification({
-						id: `logout-${getCurrentSeconds()}`,
-						message,
-						color: "teal",
-						icon: <Check size={15} />,
-					});
-				}
-			})
-			.catch((error) => {
-				// An error happened.
-				showNotification({
-					id: `logout-${getCurrentSeconds()}`,
-					message: error.message || "Something went wrong",
-					color: "red",
-					icon: <X size={15} />,
-				});
-			})
-			.finally(() => {
-				dispatch(loggingOutToggler(false));
-			});
-	};
+        // get current seconds since epoch to use as a unique id
+        const getCurrentSeconds = () => Math.floor(Date.now() / 1000);
+        signOut(Auth)
+            .then(() => {
+                if (message) {
+                    // Sign-out successful.
+                    dispatch(MakeUnAuthenticated());
+                    showNotification({
+                        id: `logout-${getCurrentSeconds()}`,
+                        message,
+                        color: "teal",
+                        icon: <Check size={15} />,
+                    });
+                }
+            })
+            .catch((error) => {
+                // An error happened.
+                showNotification({
+                    id: `logout-${getCurrentSeconds()}`,
+                    message: error.message || "Something went wrong",
+                    color: "red",
+                    icon: <X size={15} />,
+                });
+            })
+            .finally(() => {
+                dispatch(loggingOutToggler(false));
+            });
+    };
 
-	return (
-		<>
-			<DeleteUserConfirmModal
-				deleteConfirmBoxOpened={deleteConfirmBoxOpened}
-				deleteConfirmBoxToggleOpened={deleteConfirmBoxToggleOpened}
-				handleLogoutUser={handleLogoutUser}
-			/>
-			<Header height={50} className={classes.root}>
-				<Container size="xl" className={classes.header}>
-					<Group spacing={0}>
-						{smallScreen && (
-							<Burger
-								opened={opened}
-								onClick={() => setOpened((opened) => !opened)}
-								size="sm"
-							/>
-						)}
-						<Group spacing={0}>
-							<Image
-								alt="logo"
-								className={classes.logo}
-								src="/Logo.png"
-							/>
-							<Title className={classes.Title}>Expense Tracker</Title>
-						</Group>
-					</Group>
-					<AvatarMenu
-						deleteConfirmBoxToggleOpened={deleteConfirmBoxToggleOpened}
-						handleLogoutUser={handleLogoutUser}
-					/>
-				</Container>
-			</Header>
-		</>
-	);
+    return (
+        <>
+            <DeleteUserConfirmModal
+                deleteConfirmBoxOpened={deleteConfirmBoxOpened}
+                deleteConfirmBoxToggleOpened={deleteConfirmBoxToggleOpened}
+                handleLogoutUser={handleLogoutUser}
+            />
+            <Header height={50} className={classes.root}>
+                <Container size="xl" className={classes.header}>
+                    <Group spacing={0}>
+                        {smallScreen && (
+                            <Burger
+                                opened={opened}
+                                onClick={() => setOpened((opened) => !opened)}
+                                size="sm"
+                            />
+                        )}
+                        <Group spacing={0}>
+                            <Image
+                                alt="logo"
+                                className={classes.logo}
+                                src="/Logo.png"
+                            />
+                            <Title className={classes.Title}>
+                                Expense Tracker
+                            </Title>
+                        </Group>
+                    </Group>
+                    <AvatarMenu
+                        deleteConfirmBoxToggleOpened={
+                            deleteConfirmBoxToggleOpened
+                        }
+                        handleLogoutUser={handleLogoutUser}
+                    />
+                </Container>
+            </Header>
+        </>
+    );
 };
 
 export default memo(UpperNavbar);
--- src/Components/Navbar/UpperNavbar/UpperNavbar.style.js
+++ src/Components/Navbar/UpperNavbar/UpperNavbar.style.js
@@ -1,47 +1,47 @@
 import { createStyles } from "@mantine/core";
 
 const useStyles = createStyles((theme, _params, getRef) => {
-	return {
-		root: {
-			position: "sticky",
-			zIndex: 2,
-			backgroundColor: theme.colors.dark[8],
-		},
+    return {
+        root: {
+            position: "sticky",
+            zIndex: 2,
+            backgroundColor: theme.colors.dark[8],
+        },
 
-		header: {
-			display: "flex",
-			justifyContent: "space-between",
-			alignItems: "center",
-			height: "100%",
-			overflow: "hidden",
+        header: {
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            height: "100%",
+            overflow: "hidden",
 
-			"@media (min-width: 710px)": {
-				paddingLeft: 0,
-			},
-		},
-		Title: {
-			fontSize: "1.5rem",
-			fontWeight: 600,
-			color: theme.colors.dark[0],
-			transition: "color 0.2s ease",
-			fontFamily: "Oswald, serif !important",
+            "@media (min-width: 775px)": {
+                paddingLeft: 0,
+            },
+        },
+        Title: {
+            fontSize: "1.5rem",
+            fontWeight: 600,
+            color: theme.colors.dark[0],
+            transition: "color 0.2s ease",
+            fontFamily: "Oswald, serif !important",
 
-			"@media (min-width: 710px)": {
-				fontSize: "2.2rem",
-			},
+            "@media (min-width: 775px)": {
+                fontSize: "2.2rem",
+            },
 
-			"@media (max-width: 380px)": {
-				fontSize: "1rem",
-			},
-		},
+            "@media (max-width: 380px)": {
+                fontSize: "1rem",
+            },
+        },
 
-		logo: {
-			width: "100px !important",
-			"@media (max-width: 380px)": {
-				width: "80px !important",
-			},
-		},
-	};
+        logo: {
+            width: "100px !important",
+            "@media (max-width: 380px)": {
+                width: "80px !important",
+            },
+        },
+    };
 });
 
 export default useStyles;
